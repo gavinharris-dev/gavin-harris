bb8b5787ea6898225720b0dc26b7c8ad62833a35
Gavin Harris <gavin.harris@colab.com>
2021-07-14 20:07:39 +1000

    Ugh

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,4 +1,3 @@
-
 import Container from "./container";
 
 const Footer = () => (
@@ -6,14 +5,17 @@ const Footer = () => (
     <Container>
       <div className='py-10 flex flex-col lg:flex-row items-center print:hidden'>
         <h3 className='text-lg font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2 flex-grow'>
-          Hosted simply by <a href="https://vercel.com">Vercel</a>. 
+          Hosted simply by <a href='https://vercel.com'>Vercel</a>.
         </h3>
-        <span className="flex flex-row">
-          <h4 className="tracking-tighter font-light mr-2">
+        <span className='flex flex-row'>
+          <h4 className='tracking-tighter font-light mr-2'>
             View the source on
           </h4>
-          <a href="https://github.com/gavinharris-dev/gavin-harris">
-            <img src="/assets/img/GitHub-Mark/PNG/GitHub-Mark-32px.png" alt="GitHub Logo"/>
+          <a href='https://github.com/gavinharris-dev/gavin-harris'>
+            <img
+              src='/assets/img/GitHub-Mark/PNG/GitHub-Mark-32px.png'
+              alt='GitHub Logo'
+            />
           </a>
         </span>
       </div>
--- components/meta.tsx
+++ components/meta.tsx
@@ -3,9 +3,23 @@ import Head from "next/head";
 
 const Meta = () => (
   <Head>
-    <link rel='apple-touch-icon' sizes='180x180' href='/favicon/apple-touch-icon.png' />
-    <link rel='icon' type='image/png' sizes='32x32' href='/favicon/favicon-32x32.png' />
-    <link rel='icon' type='image/png' sizes='16x16' href='/favicon/favicon-16x16.png' />
+    <link
+      rel='apple-touch-icon'
+      sizes='180x180'
+      href='/favicon/apple-touch-icon.png'
+    />
+    <link
+      rel='icon'
+      type='image/png'
+      sizes='32x32'
+      href='/favicon/favicon-32x32.png'
+    />
+    <link
+      rel='icon'
+      type='image/png'
+      sizes='16x16'
+      href='/favicon/favicon-16x16.png'
+    />
     <link rel='manifest' href='/favicon/site.webmanifest' />
 
     <link
@@ -20,7 +34,7 @@ const Meta = () => (
     <link rel='alternate' type='application/rss+xml' href='/feed.xml' />
     <meta
       name='description'
-      content="Gavin Harris is an IT Professional with 20+ years experience."
+      content='Gavin Harris is an IT Professional with 20+ years experience.'
     />
     <meta property='og:image' content={HOME_OG_IMAGE_URL} />
   </Head>
